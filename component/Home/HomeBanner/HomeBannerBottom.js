c0a02c063fd1b58d9f332cc6583c650c56487c75
hardik <hardik.d@applocum.org>
2023-10-19 12:13:07 +0530

    bannerbottompart animation for image scrolling

--- component/Home/HomeBanner/HomeBannerBottom.js
+++ component/Home/HomeBanner/HomeBannerBottom.js
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const HomeBannerBottom = () => {
+  const bannerAnimation = () => {
+    if(typeof window === "undefined") {
+      return;
+    }
+    if(window.innerWidth < 768) {
+      return;
+    }
+    window.addEventListener("scroll", function () {
+      var mainDiv = document.querySelector(".bannerbottompart");
+      var mainImg = document.querySelector(".bannerbottompart .leftsideimg img");
+      var value = window.scrollY;
+      if(mainDiv) {
+        var transformValue = value / 4000 + 1;
+        if(transformValue > 1.2) {
+          transformValue = 1.2;
+        }
+        mainImg.style.transform = `scale(${transformValue})`;
+      }
+    });
+  }
+  useEffect(()=>{
+    bannerAnimation();
+  },[])
   return (
     <div className="bannerbottompart">
       <div className="leftsideimg">
